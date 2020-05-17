0498eac0ce401773ede0d10a7d220490a6fd9a16
Zoe Ludwig <zoe.ludwig@lambdaschool.com>
2020-05-17 16:22:27 -0700

    first draft index.js

---
+++ README.md
@@ -0,0 +1,99 @@
+The module challenge is the afternoon project or assignment that students work through independently. This expands on the guided project completed earlier with the instructor.
+
+## [Enter sprint name here]
+
+## [Enter module name here]
+
+## Objectives
+
+- Include each of the objectives from the module here.
+- Keeping the objectives here for reference will
+- be useful for the instructor composing the module challenge,
+- as well as for the student completing the challenge.
+
+![Ice cream](https://www.gannett-cdn.com/-mm-/b44bfa5285a2e7ec46a75f0bf8c90525537ded0d/c=120-0-2001-1414/local/-/media/2018/05/15/Cincinnati/Cincinnati/636620007227438872-GettyImages-669243898.jpg?width=534&height=401&fit=crop)
+
+## Introduction
+
+When Baskin Robins, a global chain of ice cream parlors, first opened in 1945 they served 31 flavors of Ice Cream. They have grown to almost 6,000 franchises world wide and have became famous for serving 31 flavors at a time. Since they first opened, they no longer serve the same 31 flavors, but they do always have exactly 31 ice cream flavors available.
+
+Your challenge today will be to work with Baskin Robins ice cream flavor array to access, remove, and add flavors, all while ensuring the length never exceeds 31.
+
+## Completion requirements
+
+Here you'll describe the features and tasks that the student will need to complete in order to finish the module challenge.
+
+Provide all necessary detail in either paragraph form,
+
+1. using
+2. a
+3. numbered
+4. list,
+- [ ]  or
+- [ ]  with
+- [ ]  a
+- [ ]  checklist.
+
+Whatever manner most effectively communicates the information (consider whether a specific sequence is required or if illustrating a clear visual hierarchy would be beneficial).
+
+## Stretch goals
+
+Describe the tasks and features that would demonstrate a student's exceptional mastery of these principles — going above and beyond — but that exceeds what is required by the minimum expectations of the module challenge.
+
+You can do this with full paragraphs or by using a checklist.
+
+- [ ]  or
+- [ ]  by
+- [ ]  using
+- [ ]  a
+- [ ]  checklist
+
+## Instructions
+
+You may wish to include an ordered list of step-by-steps instructions to guide the student 
+
+1. through
+2. a 
+3. sequential
+4. process.
+
+## Tests
+
+If you're including tests, describe how to run the tests and any setup required. Be as detailed as possible – testing is an area many of our students struggle with.
+
+> Test 1
+
+- Instructions
+
+## FAQs
+
+**What should I include in this section?**
+
+*Include the answers to frequently asked questions as they come up. If it is a new module challenge, try to answer a few questions you anticipate students asking. Think ahead to potentional roadblocks for an amateur.*
+
+## Resources
+
+[Assignment help](example)
+
+[Examples in a real-world context](example)
+
+[Documentation of gotchas](example)
+
+[FAQ](example)
+
+## Submission format
+
+Describe how the student should deliver the completed challenge. If it's step-by-step,
+
+1. include
+2. a
+3. numbered
+4. list.
+
+## Grading rubric
+
+If you've prepared a grading rubric that will be used to evaluate the quality of a student's work on your module challenge assignment, include any instructions or relevant links below.
+
+[Grading rubric](example)
+
+[Module challenge grading rubric](https://www.notion.so/e7b32e56ebad4f57b3521efb886f4508)
---
+++ index.js
@@ -0,0 +1,253 @@
+var originalFlavors = ["Banana Nut Fudge",
+    "Black Walnut",
+    "Burgundy Cherry",
+   "Butterscotch Ribbon",
+    "Cherry Macaron",
+    "Chocolate",
+    "Chocolate Almond",
+    "Chocolate Chip",
+    "Chocolate Fudge",
+    "Chocolate Mint",
+    "Chocolate Ribbon",
+    "Coffee",
+    "Coffee Candy",
+    "Date Nut",
+    "Eggnog",
+    "French Vanilla",
+    "Green Mint Stick",
+    "Lemon Crisp",
+    "Lemon Custard",
+    "Lemon Sherbet",
+    "Maple Nut",
+    "Orange Sherbet",
+    "Peach",
+    "Peppermint Fudge Ribbon",
+    "Peppermint Stick",
+    "Pineapple Sherbet",
+    "Raspberry Sherbet",
+    "Rocky Road",
+    "Strawberry",
+    "Vanilla",
+    "Vanilla Burnt Almond"]
+
+/* Task 1: Confirm that the array is exactly 31 flavors. Your function should accept:
+
+(1) an array 
+
+Your function should return TRUE if the length of the array is 31 and FALSE if the length of the array is NOT 31.
+
+To save you from having to count the items above, you can assume that length of originalFlavors is 31. You may also want to test your function by creating and passing an array of a different length, to make sure it returns FALSE if length is not 31. 
+
+i.e. is31Flavors(originalFlavors) will return TRUE.*/
+
+function is31Flavors(/*code here*/){
+
+    /*code here*/
+
+}
+
+/* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
+
+Your function should accept:
+
+(1) an array 
+(2) a flavor
+
+Your function should add the flavor to the front of the array and console.log the resulting array.
+
+For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
+
+function addFlavor(/*code here*/){
+
+    /*code here*/
+
+}
+
+
+/* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
+
+Your function should accept:
+
+(1) an array 
+
+Your function should remove a flavor from the end of the array and console.log the resulting array.
+
+For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
+
+function removeLastFlavor(/*code here*/){
+
+    /*code here*/
+
+}
+
+/* Task 4: Write a function that returns a flavor at a given index in the array.
+
+Your function should accept:
+
+(1) an array 
+(2) an index
+
+For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
+
+function getFlavorByIndex(/*code here*/){
+
+    /*code here*/
+
+}
+
+/* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
+
+Your function should accept: 
+
+(1) an array
+(2) a string (flavor)
+
+For example, removeFlavorByName(originalFlavors, "Vanilla") would return an array with the length 30 including all of the flavors except Vanilla. */
+
+function removeFlavorByName(/*code here*/){
+
+    /*code here*/
+
+}
+
+
+/* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called that makes a copy of the array. 
+
+Your function should accept: 
+
+(1) an array
+
+and should return a new array that is identical to the old array. You can name the new array however you'd like. */ 
+
+function copy(/*code here*/){
+
+    /*code here*/
+
+}
+
+/* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
+
+Your function should accept: 
+
+(1) an Array
+(2) a string (i.e. "chocolate")
+
+and return a new array. 
+
+For example, filterByWord(originalFlavors, "chocolate") should return ["Chocolate", "Chocolate Almond",..."Chocolate Ribbon"].
+
+DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.*/
+
+function filterByWord(/*code here*/){
+
+    /*code here*/
+
+}
+
+
+
+/* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
+
+/* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
+
+Your function should accept: 
+
+(1) an array
+
+and should return the average number of words per item in the array. 
+
+For example, getAverageWordLength(originalFlavors) should return a number between 0 and 3. */
+
+function getAverageWordLength(/*code here*/){
+
+    /*code here*/
+
+}
+
+
+/* STRETCH 2: Baskin Robins now offers new flavors, seasonal flavors, and even regional flavors. Write a function that will randomly select a total of 31 flavors from originalFlavors, currentFlavors, seasonalFlavors, and regionalFlavors.
+
+Your function should accept 4 different arrays,
+
+and should return a new array called randomFlavors with a length 31.
+
+forExample, getRandomFlavors(originalFlavors, newFlavors, seasonalFlavors, regionalFlavors) might return ["Strawberry Cheesecake", "Eggnog,"..."Chocolate"].*/
+
+// Data ⬇️
+var newFlavors = ["Date night",
+    "U.S.S Butterscotch (Stranger Things special)",
+    "Honey Almond",
+    "Mint Chocolate Chip",
+    "Chocolate",
+    "Oreo® Cookies'n Cream",
+    "Chocolate Chip",
+    "Pralines 'n Cream",
+    "Very Berry Strawberry",
+    "Chocolate Chip Cookie Dough",
+    "Old Fashioned Butter Pecan",
+    "Jamoca®",
+    "Jamoca® Almond Fudge",
+    "Reese's® Peanut Butter Cup",
+    "Rocky Road",
+    "Peanut Butter ’n Chocolate",
+    "Gold Medal Ribbon®",
+    "World Class® Chocolate",
+    "Cherries Jubilee",
+    "Chocolate Fudge",
+    "Daiquiri Ice",
+    "Rainbow Sherbet",
+    "Rainbow Swirl"] 
+var seasonalFlavors = ["America's Birthday Cake",
+    "Baseball Nut®",
+    "Blueberry Cheesecake",
+    "Bourbon Street Pecan Pie",
+    "Brownie Bar Mashup",
+    "Cherry Cordial with Kisses",
+    "Chocolate Mousse Royale",
+    "French Vanilla",
+    "Eggnog",
+    "German Chocolate Cake",
+    "Icing on the Cake",
+    "Love Potion #31",
+    "New York Cheesecake",
+    "Nutty Coconut",
+    "Peppermint",
+    "Strawberry Cheesecake",
+    "Rock ’n Pop Swirl",
+    "Reese’s Peanut Butter Cup",
+    "Trick Oreo Treat",
+    "Winter White Chocolate",
+    "made with Snickers®",
+    "made with M&M's®",
+    "Heath®",
+    "Mango Tango",]
+var regionalFlavors = ["Pink Bubblegum",
+    "Caramel Macchiato",
+    "York Peppermint Pattie",
+    "Cotton Candy",
+    "Orange Sherbet",
+    "Grape Ice",
+    "Watermelon Ice",
+    "Miami Vice Sorbet",
+    "Splish Splash®",
+    "Wild 'n Reckless Sherbet",
+    "Lemon Custard",
+    "Oregon Blackberry",
+    "Bananas ‘n Strawberries",
+    "Mississippi Mud",
+    "Rum Raisin",
+    "Creole Cream Cheese",
+    "Chocolate Almond",
+    "Fudge Brownie",
+    "Banana Nut",
+    "Black Walnut",
+    "Cotton Candy Crackle",
+    "Quarterback Crunch",
+    "Chocolate Chocolate Chip Cheesecake",
+    "Caramel 'n' Cookies"]
+
+function getRandomFlavors(/*code here*/){
+
+    /*code here*/
+
+}
