19f4ac0443cc874d651252799f14e59d233d1af7
Ava Wingfield <avelinewingfield@gmail.com>
2020-06-10 09:56:53 -0400

    OH am I fussygit add .

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ function is31Flavors(arr){
     }
 
     /*code here*/
-is31Flavors(originalFlavors);
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -69,7 +68,6 @@ return arr;
     /*code here*/
 }
 
-addFlavor(originalFlavors, "Rainbow Sherbet");
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
@@ -89,7 +87,6 @@ function removeLastFlavor(arr){
 }
     /*code here*/
 
-removeLastFlavor(originalFlavors);
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
 Your function should accept:
@@ -105,7 +102,6 @@ return arr[idx];
 
 }
 
-getFlavorByIndex(originalFlavors, 2);
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -126,7 +122,6 @@ function removeFlavorByName(arr, str){
     /*code here*/
 
 }
-removeFlavorByName(originalFlavors, "vanilla")
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -139,9 +134,12 @@ and should return a new array that is identical to the old array. You can name t
 
 function copy(newArr, originalArr){
 
-    /*code here*/
-
+    newArr = originalArr.slice();
+    console.log(newArr)
+    return newArr
 }
+let originalOriginalFlavors = []
+copy(originalOriginalFlavors, originalFlavors);
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -170,7 +168,6 @@ function filterByWord(arr, str){
     }return newArr
 }
 
-filterByWord(originalFlavors, "Chocolate");
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
@@ -195,7 +192,6 @@ return itemLength/counter;
     /*code here*/
 
 }
-getAverageWordLength(originalFlavors)
 
 
 /* STRETCH 2: Baskin Robins now offers new flavors, seasonal flavors, and even regional flavors. Write a function that will randomly select a total of 31 flavors from originalFlavors, currentFlavors, seasonalFlavors, and regionalFlavors.
