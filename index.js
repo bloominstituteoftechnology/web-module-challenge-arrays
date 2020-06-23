4a797e8f5db463a379105a4a6a5f004d74d4a36b
Angie Lawrence <43270346+Angie0488@users.noreply.github.com>
2020-06-22 21:13:03 -0400

    Completed Task 1

--- index.js
+++ index.js
@@ -40,11 +40,15 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
+function is31Flavors(originalFlavors) { 
 
-    /*code here*/
-
+    if (originalFlavors.length === 31) {
+        console.log("True");
+    } else {
+        console.log("False");
+    }
 }
+is31Flavors(originalFlavors);
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
