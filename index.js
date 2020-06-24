c86360253acf333fef13d25a947cc32f8f522b66
extrude57 <gamemeup8@protonmail.com>
2020-06-23 20:25:47 -0700

    Testing the functions

--- index.js
+++ index.js
@@ -74,8 +74,9 @@ arr.forEach( r => console.log(r));
 
 }
 
-
-/* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
+addFlavor('What', originalFlavors);
+/* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item
+ from the end of the array. 
 
 Your function should accept:
 
@@ -92,7 +93,8 @@ return arr;
     
 
 }
-
+let f = removeLastFlavor(originalFlavors);
+f.forEach(r => console.log(r));
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
 Your function should accept:
@@ -109,7 +111,9 @@ function getFlavorByIndex(arr,indx){
     return v;
 
 }
-
+let indexnum = 1;
+let whatflavor = getFlavorByIndex(originalFlavors,indexnum);
+console.log(indexnum + " is the "+ whatflavor);
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based
  on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and
   remove that flavor from the array. 
