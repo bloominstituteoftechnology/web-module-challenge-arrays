f4c13974c91a9b64d1179f018efbb222904b1cbb
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-07-28 13:16:43 -0700

    task6

--- index.js
+++ index.js
@@ -142,11 +142,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(a,b){
+function copy(newArray,originalFlavors){
 
-    /*code here*/
+newArray = originalFlavors.slice();
 
 }
+
+copy(newArray)
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
