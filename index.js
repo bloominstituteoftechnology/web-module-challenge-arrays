2e010fced4d78f73fe6465c9419f2896f5f8d0a9
Samantha Barney <barney-samantha@lambdastudents.com>
2020-07-28 23:05:38 -0400

    task 4 flavor by index

--- index.js
+++ index.js
@@ -100,12 +100,7 @@ For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", a
 
 function getFlavorByIndex(array, index){
     return array[index]
-}
     console.log('get flavor by index' getFlavorByIndex(originalFlavors,2))
-
-
-    
-
 }
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
