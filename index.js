120a39b7a2937e6afd99573aaa69511de79817a5
rldouglas <ashiatsustl07@gmail.com>
2020-08-27 21:58:22 -0500

    task 7

--- index.js
+++ index.js
@@ -63,10 +63,13 @@ Your function should add the flavor to the front of the array and console.log th
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
 function addFlavor(array, flavor){
-    return(addFlavor==="RainbowSherbert", originalFlavors)
-        }            
+        array.unshift("RainbowSherbert")
 
-console.log(addFlavor("RainbowSherbert"));
+    return array
+        }   
+                 
+
+console.log(addFlavor(originalFlavors, "RainbowSherbert"));
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
@@ -79,11 +82,13 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(array, RainbowSherbert)
-{   return(removeLastFlavor==="RainbowSherbert", originalFlavors)
+function removeLastFlavor(array){
+    array.pop()
+    return array
 
 }   
-console.log(removeLastFlavor("RainbowSherbert"(originalFlavors));
+console.log(removeLastFlavor(originalFlavors)
+);
     /*code here*/
 
 
@@ -97,11 +102,13 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(array, index){
+    return array[index]
 
     /*code here*/
 
-}
+} 
+console.log(getFlavorByIndex(originalFlavors, 2))
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
@@ -116,11 +123,14 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
+function removeFlavorByName(array, string){
+     array.pop("Vanilla")
+     return array  
 
     /*code here*/
 
 }
+console.log(removeFlavorByName(originalFlavors, "Vanilla"));
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -131,11 +141,15 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(array){
+    array.copyWithin("newFlavors")
+    return array
 
     /*code here*/
 
 }
+console.log(copy(originalFlavors, newFlavors))
+
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -152,12 +166,11 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(array, string){
+    array.includes("chocolate")
+    return new array
 }
-
+console.log(filterByWord[originalFlavors, "chocolate"]);
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
@@ -260,8 +273,8 @@ var regionalFlavors = ["Pink Bubblegum",
     "Chocolate Chocolate Chip Cheesecake",
     "Caramel 'n' Cookies"]
 
-function getRandomFlavors(/*code here*/){
+function getRandomFlavors(/*code here*/){}
 
     /*code here*/
 
-}
+
