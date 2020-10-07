dd8bb9ea5bf9fd74a2ea6aa4a57488e071033daa
Caudill-Z <71600918+Caudill-Z@users.noreply.github.com>
2020-10-07 13:18:52 -0500

    addFlavor function created

--- index.js
+++ index.js
@@ -84,8 +84,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(array, flavor){
+   array.unshift(flavor);
+   return array;
 }
 
 
