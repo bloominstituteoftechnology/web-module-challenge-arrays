a8315e9cb5747f0ee017e67733ae3e0566d18d6a
Caudill-Z <71600918+Caudill-Z@users.noreply.github.com>
2020-10-07 14:05:04 -0500

    filterByWord completed - assignment done

--- index.js
+++ index.js
@@ -163,8 +163,14 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(array, string){
+    let filteredArray = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i].includes(string)){
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
 }
 
 
