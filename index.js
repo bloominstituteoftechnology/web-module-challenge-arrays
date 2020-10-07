9be3fe06c7d7443fb4196d94e3c06588c0ecdeb7
Brit Hemming <hello@brittanyhemming.com>
2020-10-07 14:14:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ With all of these changes going on, we don't want to lose track of the actual, o
 
 /*
 Use the copy function below to do the following:
-    1. Receive two arguments: one for your new array and one for your original array
-    2. Return the new array that holds an exact copy of the old array  
+    1. An array as an argument
+    2. Return a copy of the received array  
 */
 
 function copy(/*your code here*/){
