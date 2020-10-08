c662ca4e96e6f705ef0ba035edf588be77708195
donhueypaul <55775606+donhueypaul@users.noreply.github.com>
2020-10-07 22:21:00 -0700

    Solved 3 questions

--- index.js
+++ index.js
@@ -64,8 +64,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(arr){
+    if (arr.length === 31) {
+      return true  
+    } else {
+        return false
+    }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
