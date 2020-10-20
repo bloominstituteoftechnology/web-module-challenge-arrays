50df28846fb87a85535645c87453439df4289e0c
AviasNelan <AviasNelan@users.noreply.github.com>
2020-10-20 18:42:57 -0500

    up to 4 passed tests

--- index.js
+++ index.js
@@ -45,8 +45,10 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(){
+    let newFlavors = originalFlavors.slice(0, 32);
+    return newFlavors;
+
 }    
 
 
@@ -64,9 +66,11 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(flavorArray) {
+    (flavorArray.length === 31);
+
 }
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,8 +85,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(newFlavorsSetup){
+   newFlavorsSetup.unshift("Rainbow Sherbert");
+   return newFlavorsSetup;
 }
 
 
@@ -97,8 +102,10 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(newFlavorsSetup){
+   newFlavorsSetup.pop();
+   return newFlavorsSetup;
+   
 }
 
 
@@ -113,8 +120,9 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(newFlavorsSearch, []){
+    newFlavorsSearch.slice();
+    return newFlavorsSearch;
 }
 
 
