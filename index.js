21e097193db23fcc206de49376685b44db74f464
orlando taylor <ackerie_taylor@hotmail.com>
2020-10-20 23:58:17 -0400

    1st attemp

--- index.js
+++ index.js
@@ -45,8 +45,14 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+
+function copy(originalFlavors){
+
+let flavorCopy = Array.from(originalFlavors)
+
+return flavorCopy;
+
+
 }    
 
 
@@ -63,10 +69,16 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
+// 
+function is31Flavors(originalFlavors){
+   if (originalFlavors.length === 31){
+       return true;
+          } else {
+              return false;
+          }
+        }
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
-}
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,10 +93,13 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
-}
+function addFlavor(originalFlavors){
+      originalFlavors.unshift('Rainbow Sherbert');
+    
+    return originalFlavors;
 
+}
+// addFlavor('Rainbow Sherbert');
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
@@ -97,10 +112,11 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
-}
+function removeLastFlavor(originalFlavors){
+  originalFlavors.pop();
 
+  return originalFlavors;
+}
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -113,8 +129,9 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(originalFlavors, index){
+   
+    return originalFlavors[index];
 }
 
 
@@ -132,8 +149,11 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(originalFlavors){
+  
+    originalFlavors.splice(originalFlavors ('Rocky Road'), 1);
+
+    return originalFlavors;
 }
 
 
