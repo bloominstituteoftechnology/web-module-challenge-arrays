fa3235a2c8ff0e93f75d6ec94e0391ed7fed3f91
Leah Tabush <leahtabush@gmail.com>
2020-11-04 00:08:57 -0800

    early commit to make sure CodeGrade is connected properly.

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(originalArray){
+  var newArray = originalArray;
+  return newArray;
 }    
 
 
@@ -64,8 +65,8 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(arr){
+   return arr.length === 31;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
