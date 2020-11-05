35b46157b33db9848c8276390c478346424a310a
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2020-11-04 22:06:37 -0800

    complete task 1

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy(array){
+    let arrayCopy = [...array];
+    
+    return arrayCopy 
+}
+console.log(copy(originalFlavors));  
 
 
 
