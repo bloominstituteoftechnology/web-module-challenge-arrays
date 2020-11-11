22f73d0e72c56dca36e75f4b8295f34cecd17be2
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2020-11-11 11:39:03 -0800

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -44,13 +44,9 @@ Use the copy function below to do the following:
     1. An array as an argument
     2. Return a copy of the received array  
 */
-
-function copy([originalFlavors]){
-    return [...originalFlavors]
+function copy(originalFlavors){
+    const originalFlavors = [];
 }    
-  copy([originalFlavors])
-
-
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -65,8 +61,9 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 
 
 function is31Flavors(/*your code here*/){
-   /*your code here*/
-}
+    /*your code here*/
+ }
+ 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
