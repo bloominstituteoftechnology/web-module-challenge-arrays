fbd901e6342e7ad72bb63163dbc317abbdd41c86
Trevor Standridge <trvrstndrdg@gmail.com>
2020-11-17 15:49:58 -0800

    Task 7 under review

--- index.js
+++ index.js
@@ -169,10 +169,15 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(array, flavor){
+    // if (array.includes(flavor) === true){
+        let filteredArray = array.map(n => n.includes(flavor))
+        console.log(filteredArray);
+    // } else 
+        // return console.log(flavor);
 }
 
+filterByWord(originalFlavors, "Chocolate")
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
