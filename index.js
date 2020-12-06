03bab67fe27ece78b76680adc95c460f1b0ba6b9
TwkRbell3489 <73049625+TwkRbell3489@users.noreply.github.com>
2020-12-06 03:26:27 -0600

    begginig

--- index.js
+++ index.js
@@ -45,8 +45,38 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(const array ="Banana Nut Fudge",
+"Black Walnut",
+"Burgundy Cherry",
+"Butterscotch Ribbon",
+"Cherry Macaron",
+"Chocolate",
+"Chocolate Almond",
+"Chocolate Chip",
+"Chocolate Fudge",
+"Chocolate Mint",
+"Chocolate Ribbon",
+"Coffee",
+"Coffee Candy",
+"Date Nut",
+"Eggnog",
+"French Vanilla",
+"Green Mint Stick",
+"Lemon Crisp",
+"Lemon Custard",
+"Lemon Sherbet",
+"Maple Nut",
+"Orange Sherbet",
+"Peach",
+"Peppermint Fudge Ribbon",
+"Peppermint Stick",
+"Pineapple Sherbet",
+"Raspberry Sherbet",
+"Rocky Road",
+"Strawberry",
+"Vanilla",
+"Vanilla Burnt Almond")
+
 }    
 
 
