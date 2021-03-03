7ff68acd62e5c40ccee45d96b9dfd1251f098398
Marcus Flores <flresmarcus@gmail.com>
2021-03-02 19:29:27 -0800

    Finished MVP

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(inputArr){
+    var copyArr = inputArr;
+    return copyArr;
 }    
 
 
@@ -64,8 +65,11 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(inputArr){
+    if(inputArr.length == 31){
+        return true;
+    }
+    else return false;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -81,8 +85,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(inputArr, inputFlavor){
+   inputArr.unshift(inputFlavor);
+   return inputArr;
 }
 
 
@@ -97,8 +102,9 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(inputArr){
+   inputArr.pop();
+   return inputArr;
 }
 
 
@@ -114,8 +120,8 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(inputArr, indexNum){
+    return inputArr[indexNum];
 }
 
 
@@ -134,8 +140,14 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(inputArr, inputFlavor){
+    var indexNum;
+    if(inputArr.includes(inputFlavor)){
+        indexNum = inputArr.findIndex(inputFlavor);
+        inputArr.splice(indexNum, 1);
+        return inputArr;
+    }
+    else return null;
 }
 
 
@@ -160,8 +172,14 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(inputArr, inputFilter){
+    var returnArr = [];
+    for(var i = 0; i < inputArr.length; i++){
+        if(inputArr[i].includes(inputFilter)){
+            returnArr.push(inputArr[i]);
+        }
+    }
+    return returnArr;
 }
 
 
