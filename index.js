880c204aba5f7fac8a701ea30d22ece38bb5eec7
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-03-03 13:01:41 -0600

    finished with task 5, moving on to task 6

--- index.js
+++ index.js
@@ -120,8 +120,8 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(arr, num){
+    return arr[num];
 }
 
 
