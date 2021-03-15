67239c0288a7671380426ab4fd4f3c0fd43e5822
Zack-Ballenger <zack.ballenger@tutanota.com>
2021-03-15 08:53:54 -0600

    Got 5 to pass, trying to get the last three

--- index.js
+++ index.js
@@ -114,8 +114,9 @@ Use the removeLastFlavor function below to do the following:
     For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
-   /*your code here*/
+function removeLastFlavor(array){
+   array.pop();
+   return array;
 }
 
 
@@ -131,8 +132,13 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+function getFlavorByIndex(array, index)  {
+for(let i = 0; i <array.length; i++); {
+    if (i === num) {
+        return array [i];
+    }
+}
+   
 }
 
 
@@ -151,8 +157,13 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(array, microFiber){
+    for(let i = 0; i <array.length; i++); {
+        if (array[i] === microFiber) {
+            array.splice(i,1);
+            return array;
+        }
+    }
 }
 
 
