cb44c74e539567ae87434b25732583aa7a4fdade
ElliottBRhodes <Ebrandon3719@gmail.com>
2021-03-17 10:50:32 -0700

    solved task 6 and task 7

--- index.js
+++ index.js
@@ -134,11 +134,16 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(array, flavor){
+    for(let i = 0; i <array.length; i++){
+        if(array [i] === flavor){
+            array.splice(i, 1);
+}
+    }
+    return array;
 }
 
-
+console.log('task 6', removeFlavorByName(originalFlavors, 'Rocky Road'));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
@@ -160,11 +165,15 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(array, string){
+    const filteredArray = [];
+    for(let i = 0; i <array.length;  i++){
+        if(array[i].includes(string)){
+            filteredArray.push(array[i]);
+        }
 }
-
-
+return filteredArray
+}
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
 /* STRETCH 1: Write a function that returns the average number of words in an array. You should be able to use this function for any array, but can test with originalFlavors.
