bdf4ae670bab6c27e2042927fd205700555a8f0e
MarkelWhit <litslayer@gmail.com>
2021-04-14 13:03:02 -0500

    checking assignment setup

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(array){
+    return array;
 }    
 
-
+copy(originalFlavors)
 
 
 
