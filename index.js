bc7b436cd223148be7bc04296efbf5169daa4a72
IfeIge <79335183+IfeIge@users.noreply.github.com>
2021-04-14 14:34:07 -0400

    commit to check if coecheck works

--- index.js
+++ index.js
@@ -119,9 +119,13 @@ Use the getFlavorByIndex function below to do the following:
     For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-    /*your code here*/
+
+function getFlavorByIndex(array, index){
+    return array[index];
 }
+
+getFlavorByIndex(originalFlavors, 2)
+
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -139,7 +143,7 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
+function removeFlavorByName(){
     /*your code here*/
 }
 
