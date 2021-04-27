1467852dc7fef0c30aded4815c5e68cd2ca11414
Eric <Eric@erics-air.lan>
2021-04-27 17:12:17 -0500

    completed Task 3

--- index.js
+++ index.js
@@ -121,10 +121,53 @@ Use the addFlavor function below to do the following:
     For example: addFlavor(originalFlavors, "Rainbow Sherbert") should return the array ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"]
 */
 
+let Flavors = [
+    "Banana Nut Fudge",
+    "Black Walnut",
+    "Burgundy Cherry",
+    "Butterscotch Ribbon",
+    "Cherry Macaron",
+    "Chocolate",
+    "Chocolate Almond",
+    "Chocolate Chip",
+    "Chocolate Fudge",
+    "Chocolate Mint",
+    "Chocolate Ribbon",
+    "Coffee",
+    "Coffee Candy",
+    "Date Nut",
+    "Eggnog",
+    "French Vanilla",
+    "Green Mint Stick",
+    "Lemon Crisp",
+    "Lemon Custard",
+    "Lemon Sherbet",
+    "Maple Nut",
+    "Orange Sherbet",
+    "Peach",
+    "Peppermint Fudge Ribbon",
+    "Peppermint Stick",
+    "Pineapple Sherbet",
+    "Raspberry Sherbet",
+    "Rocky Road",
+    "Strawberry",
+    "Vanilla",
+    "Vanilla Burnt Almond"
+]
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+const newFlavor = 'Rainbow Sherbert';
+
+
+function addFlavor(Flavors , newFlavor){
+    Flavors.unshift('Rainbow Sherbert');
+    return Flavors
+
 }
+addFlavor(Flavors, 'Rainbow Sherbert');
+
+
+console.log(addFlavor);
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
