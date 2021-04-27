4443531d6a03244272943612fe4dec985a80fecf
Gtorres21 <giovannytorres21@gmail.com>
2021-04-27 14:43:19 -0700

    Completed task 1 mVP

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(array){
+    return array;
 }    
-
+console.log(originalFlavors);
 
 
 
