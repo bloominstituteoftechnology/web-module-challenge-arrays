ff33c166c6f7d0993793f3594be560b0871905f6
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-04-28 07:15:15 -0500

    edit

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ Use the getFlavorByIndex function below to do the following:
 
 function getFlavorByIndex(originalFlavors) {
     console.log(originalFlavors)
-    let pos = originalFlavors.indexOf(4)
+    let pos = originalFlavors.indexOf("Burgundy Cherry")
     console.log(pos)
     return originalFlavors
 
