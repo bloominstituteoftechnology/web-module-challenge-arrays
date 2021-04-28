047c72a37c79ed6d14fcd0e4c6c734c2f074b853
unknown <braxtonsislo@gmail.com>
2021-04-27 21:59:51 -0500

    done

--- index.js
+++ index.js
@@ -163,8 +163,8 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/) {
-    /*your code here*/
+function filterByWord(originalFlavors) {
+    return filterByWord !== '';
 }
 
 
