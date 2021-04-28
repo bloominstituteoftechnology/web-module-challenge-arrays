ab906b968c5a6a90099e3475a94e493ba3fa079f
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-04-28 05:12:13 -0500

    edit

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ Use the addFlavor function below to do the following:
 
 
 function addFlavor(originalFlavors) {
-     newFlavors = ["Rainbow Sherbert"] + originalFlavors
+    console.log (newFlavors = ["Rainbow Sherbert"] + originalFlavors)
 
     return newFlavors.unshift
 }  
