5fcf1a8924800e4082b6230358365db86bc54c73
Bennett Kautz <bennettkautz@gmail.com>
2022-02-09 15:40:36 -0500

    finished task 1

--- index.js
+++ index.js
@@ -45,12 +45,12 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  const newArray = array.slice();
+  return newArray;
 }    
 
-
-
+copy(originalFlavors);
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
